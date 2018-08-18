4c517148f744a9d5b761526ff788bb4375f90ec2
Aldo Garces <aegarcesc@unal.edu.co>
2018-08-18 02:42:15 -0500

    modelo de kmeans no supervisado tener en cuenta la generacion de la curva ROC

--- scripts/algo.py
+++ scripts/algo.py
@@ -1,10 +1,31 @@
 from sklearn.cluster import KMeans
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+import etl as etl
 
-def kmeans(dataset):
-    results = KMeans(n_clusters=2, random_state=0).fit(dataset)
-    return results
+class algoritmos():
+    def __init__(self):
+        data = etl.leer_datos()
+        self.preparar_data(data)
+        self.modelo()
+        #print(self.X_train_real.describe())
 
-def knn(dataset):
-    neigh = KNeighborsClassifier(n_neighbors=3)
-    neigh.fit(dataset)
+    def preparar_data(self,data):
+        X_train_real, X_test_real = train_test_split(data, test_size=0.20)
+        X_train_real = X_train_real.drop('Unnamed: 0', axis=1)
+        X_train_real = X_train_real.drop('v_0', axis=1)
+        X_test_real = X_test_real.drop('Unnamed: 0', axis=1)
+        X_test_real = X_test_real.drop('v_0', axis=1)
+        self.X_train = normalize(X_train_real)
+        self.X_test = normalize(X_test_real)
+        self.X_test_real = X_test_real
+        self.X_train_real = X_train_real
+
+    def modelo(self):
+        self.results = KMeans(n_clusters=2, random_state=0).fit(self.X_train)
+        self.training_train = self.results.predict(self.X_train)
+        self.training_test = self.results.predict(self.X_test)
+        self.X_test_real['result'] = self.training_test
+        self.X_train_real['result'] = self.training_train
+        self.X_test_real.to_csv("result_test.csv")
+        self.X_train_real.to_csv("result_train.csv")
--- scripts/etl.py
+++ scripts/etl.py
@@ -1,3 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-pd.read_csv("dataset_01.csv")
+
+def leer_datos():
+    '''
+    esta funcion el trabajo que realiza es cargar la informacion en la variable data, e imprimir las relaciones entre las columnas
+    'v_1', 'v_2', 'v_3', 'v_4', 'v_5', 'v_6', 'v_7', 'v_8', 'v_9', 'v_10', 'v_11', 'v_12'
+    :return:
+    '''
+    data = pd.read_csv("../dataset/hackaton_training_v1.csv")
+    plt.matshow(data.corr())
+    plt.show()
+    print(data[['v_1', 'v_2', 'v_3', 'v_4', 'v_5', 'v_6', 'v_7', 'v_8', 'v_9', 'v_10', 'v_11', 'v_12']].corr())
+    return data
